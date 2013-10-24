7fd7cf91b5fca22a24a1aadaed99a7e65a66a595
Nimit Maru <nimit@yealthy.com>
2013-10-24 15:36:43 -0400

    init commit

---
+++ game.js
@@ -0,0 +1,46 @@
+function GameOfLife(width,height) {
+  this.width = width;
+  this.height = height;
+}
+
+GameOfLife.prototype.createAndShowBoard = function () {
+  var goltable = document.createElement("table");
+  
+  var tablehtml = '';
+  
+  for (var h=0; h<this.height; h++) {
+    tablehtml += "<tr id='row+" + h + "'>";
+    for (var w=0; w<this.width; w++) {
+      tablehtml += "<td id='" + w + "-" + h + "'></td>";
+    }
+    tablehtml += "</tr>";
+  }
+  
+  goltable.innerHTML = tablehtml;
+  var board = document.getElementById('board');
+  board.appendChild(goltable);
+  this.setupBoardEvents(board);
+};
+
+GameOfLife.prototype.setupBoardEvents = function() {
+  // each board cell has an CSS id in the format of: "x-y" 
+  // where x is the x-coordinate and y the y-coordinate
+  // use this fact to loop through all the ids and assign
+  // them "on-click" events that allow a user to click on 
+  // cells to setup the initial state of the game
+  // before clicking "Step" or "Auto-Play"
+  
+  // clicking on a cell should toggle the cell between "alive" & "dead"
+  // for ex: an "alive" cell be colored "blue", a dead cell could stay white
+};
+
+GameOfLife.prototype.step = function () {
+  
+};
+
+startGame = function () {
+  var gol = new GameOfLife(20,20);
+  gol.createAndShowBoard();
+};
+
+startGame();
