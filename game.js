4d6753f15ea5307e524b7219bd849da8a45ad276
Nimit Maru <nimit@fullstackacademy.com>
2015-03-09 11:00:51 -0400

    updates

--- game.js
+++ game.js
@@ -1,89 +1,89 @@
-function GameOfLife(width,height) {
-  this.width = width;
-  this.height = height;
-}
+var gameOfLife = {
+  width: 12,
+  height: 12,
+  stepInterval: null,
 
-GameOfLife.prototype.createAndShowBoard = function () {
-  // create <table> element
-  var goltable = document.createElement("tbody");
-  
-  // build Table HTML
-  var tablehtml = '';
-  for (var h=0; h<this.height; h++) {
-    tablehtml += "<tr id='row+" + h + "'>";
-    for (var w=0; w<this.width; w++) {
-      tablehtml += "<td data-status='dead' id='" + w + "-" + h + "'></td>";
-    }
-    tablehtml += "</tr>";
-  }
-  goltable.innerHTML = tablehtml;
-  
-  // add table to the #board element
-  var board = document.getElementById('board');
-  board.appendChild(goltable);
-  
-  // once html elements are added to the page, attach events to them
-  this.setupBoardEvents();
-};
-
-GameOfLife.prototype.forEachCell = function (iteratorFunc) {
-  /* 
-    Write forEachCell here. You will have to visit
-    each cell on the board, call the "iteratorFunc" function,
-    and pass into func, the cell and the cell's x & y
-    coordinates. For example: iteratorFunc(cell, x, y)
-  */
-};
-  
-GameOfLife.prototype.setupBoardEvents = function() {
-  // each board cell has an CSS id in the format of: "x-y" 
-  // where x is the x-coordinate and y the y-coordinate
-  // use this fact to loop through all the ids and assign
-  // them "on-click" events that allow a user to click on 
-  // cells to setup the initial state of the game
-  // before clicking "Step" or "Auto-Play"
-  
-  // clicking on a cell should toggle the cell between "alive" & "dead"
-  // for ex: an "alive" cell be colored "blue", a dead cell could stay white
-  
-  // EXAMPLE FOR ONE CELL
-  // Here is how we would catch a click event on just the 0-0 cell
-  // You need to add the click event on EVERY cell on the board
-  
-  var onCellClick = function (e) {
-    // QUESTION TO ASK YOURSELF: What is "this" equal to here?
+  createAndShowBoard: function () {
+    // create <table> element
+    var goltable = document.createElement("tbody");
     
-    // how to set the style of the cell when it's clicked
-    if (this.getAttribute('data-status') == 'dead') {
-      this.className = "alive";
-      this.setAttribute('data-status', 'alive');
-    } else {
-      this.className = "dead";
-      this.setAttribute('data-status', 'dead');
+    // build Table HTML
+    var tablehtml = '';
+    for (var h=0; h<this.height; h++) {
+      tablehtml += "<tr id='row+" + h + "'>";
+      for (var w=0; w<this.width; w++) {
+        tablehtml += "<td data-status='dead' id='" + w + "-" + h + "'></td>";
+      }
+      tablehtml += "</tr>";
     }
-  };
+    goltable.innerHTML = tablehtml;
+    
+    // add table to the #board element
+    var board = document.getElementById('board');
+    board.appendChild(goltable);
+    
+    // once html elements are added to the page, attach events to them
+    this.setupBoardEvents();
+  },
+
+  forEachCell: function (iteratorFunc) {
+    /* 
+      Write forEachCell here. You will have to visit
+      each cell on the board, call the "iteratorFunc" function,
+      and pass into func, the cell and the cell's x & y
+      coordinates. For example: iteratorFunc(cell, x, y)
+    */
+  },
   
-  var cell00 = document.getElementById('0-0');
-  cell00.onclick = onCellClick;
+  setupBoardEvents: function() {
+    // each board cell has an CSS id in the format of: "x-y" 
+    // where x is the x-coordinate and y the y-coordinate
+    // use this fact to loop through all the ids and assign
+    // them "on-click" events that allow a user to click on 
+    // cells to setup the initial state of the game
+    // before clicking "Step" or "Auto-Play"
+    
+    // clicking on a cell should toggle the cell between "alive" & "dead"
+    // for ex: an "alive" cell be colored "blue", a dead cell could stay white
+    
+    // EXAMPLE FOR ONE CELL
+    // Here is how we would catch a click event on just the 0-0 cell
+    // You need to add the click event on EVERY cell on the board
+    
+    var onCellClick = function (e) {
+      // QUESTION TO ASK YOURSELF: What is "this" equal to here?
+      
+      // how to set the style of the cell when it's clicked
+      if (this.getAttribute('data-status') == 'dead') {
+        this.className = "alive";
+        this.setAttribute('data-status', 'alive');
+      } else {
+        this.className = "dead";
+        this.setAttribute('data-status', 'dead');
+      }
+    };
+    
+    var cell00 = document.getElementById('0-0');
+    cell00.onclick = onCellClick;
+  },
+
+  step: function () {
+    // Here is where you want to loop through all the cells
+    // on the board and determine, based on it's neighbors,
+    // whether the cell should be dead or alive in the next
+    // evolution of the game. 
+    //
+    // You need to:
+    // 1. Count alive neighbors for all cells
+    // 2. Set the next state of all cells based on their alive neighbors
+    
+  },
+
+  enableAutoPlay: function () {
+    // Start Auto-Play by running the 'step' function
+    // automatically repeatedly every fixed time interval
+    
+  }
 };
 
-GameOfLife.prototype.step = function () {
-  // Here is where you want to loop through all the cells
-  // on the board and determine, based on it's neighbors,
-  // whether the cell should be dead or alive in the next
-  // evolution of the game. 
-  //
-  // You need to:
-  // 1. Count alive neighbors for all cells
-  // 2. Set the next state of all cells based on their alive neighbors
-  
-};
-
-GameOfLife.prototype.enableAutoPlay = function () {
-  // Start Auto-Play by running the 'step' function
-  // automatically repeatedly every fixed time interval
-  
-};
-
-var gol = new GameOfLife(20,20);
-gol.createAndShowBoard();
+  gameOfLife.createAndShowBoard();
